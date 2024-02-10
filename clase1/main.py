3db04852a17271aba99bcb2fe30078d7e1033ce8
flama23 <159632435+alexcordoba23@users.noreply.github.com>
2024-02-10 16:06:00 +0000

    lo logre

--- clase1/main.py
+++ clase1/main.py
@@ -1,1 +1,12 @@
-print ("primer codigo en phyton")
+class MotorElectrico:
+    Potencia= None 
+
+    def encender (self):
+        print ("Encendido motor electrico")
+
+    def __init__(self):
+        pass
+
+motor1= MotorElectrico()
+motor1.potencia= 1000
+motor1.encender()    
